573f2da26509a706791f0becf465513355236b59
mangisa <mangisa@hotmail.es>
2021-08-09 13:09:06 +0200

    Create User security

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -14,7 +14,18 @@ class DefaultController extends AbstractController
     public function index(): Response
     {
         return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
+            'my_name' => $this->getParameter('app.admin_name'),
+            'my_mail' => $this->getParameter('app.admin_email'),
+        ]);
+    }
+
+    /**
+     * @Route("/home", name="home")
+     */
+    public function home(): Response
+    {
+        return $this->render('default/home.html.twig', [
+            'my_name' => $this->getParameter('app.admin_name'),
             'my_mail' => $this->getParameter('app.admin_email'),
         ]);
     }
